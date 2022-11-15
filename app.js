328bc28dc681c45d52c1f4d795fef502e4d1a1bc
Justin <justinz@yeah.net>
2022-11-15 17:46:14 +0800

    更新

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var os = require("os");
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
